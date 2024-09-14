de0bb4b10dfc96619734f1ed2f159084b4f29321
Antonello Izzi <ai9840.github@gmail.com>
2024-09-14 17:05:58 +0000

    refactor: Better check of failure while reading athletes.

--- tests/test_athlete.py
+++ tests/test_athlete.py
@@ -20,11 +20,15 @@ def test_athlete_init():
 
 def test_athlete_getAll():
     """Returns all the athletes in the database"""
+    athlete = Athlete()
     athletes = pd.DataFrame()
     db = st.connection('memory_db', type='sql')
+    # Check no athlete table
+    with pytest.raises(DatabaseError):
+        athlete.getAll(db)
+    # Add athletes in the database
     execute_query(db, 'CREATE TABLE IF NOT EXISTS athletes (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, birthday TEXT);')
     execute_query(db, 'INSERT INTO athletes (name, birthday) VALUES ("A1", "B1"), ("A2", "B2")')
-    athlete = Athlete()
     athletes = athlete.getAll(db)
     # Check that all the expected athletes are returned
     assert athletes.loc[0]["name"] == "A1"
@@ -33,7 +37,3 @@ def test_athlete_getAll():
     assert athletes.loc[1]["birthday"] == "B2"
     # Check that nothing else is returned
     assert athletes.shape[0] == 2
-    # Check no database
-    db = st.connection('no_db', type='sql')
-    with pytest.raises(DatabaseError):
-        athlete.getAll(db)
